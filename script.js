67432701e3365a5d5ba41390e29db85831d1f937
andrzej.byc <andrzej.byc@grinn-global.com>
2023-06-15 23:04:33 +0200

    basic website look

---
+++ script.js
@@ -0,0 +1,5 @@
+function lastModified() {
+    var modiDate = new Date(document.lastModified);
+    var showAs = modiDate.getDate() + "-" + (modiDate.getMonth() + 1) + "-" + modiDate.getFullYear();
+    return showAs
+}
