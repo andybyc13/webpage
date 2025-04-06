7b22161764eeef3180c72868f4455629f68303cf
andybyc13 <45917541+andybyc13@users.noreply.github.com>
2025-04-07 00:06:21 +0200

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-function lastModified() {
-    var modiDate = new Date(document.lastModified);
-    var showAs = modiDate.getDate() + "-" + (modiDate.getMonth() + 1) + "-" + modiDate.getFullYear();
-    return showAs
-}
-
-function nextPage(pageName){
-    window.location.href = pageName;
-}
